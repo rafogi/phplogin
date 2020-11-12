4564ed4228faaf1677f8bf23f4070d4714a4f463
Raphael Cordero <68399853+rafogi@users.noreply.github.com>
2020-11-12 13:22:30 -0800

    fix folder structure

--- contact.php
+++
@@ -1,6 +0,0 @@
-<?php
-  include_once 'header.php';
-  ?>
-<h1 style='margin-top:2em'>contact</h1>
-</body>
-</html>
--- header.php
+++ header.php
@@ -12,7 +12,6 @@
 <nav class="topnav">
   <a class="active" href="index.php">Home</a>
   <a href="characters.php">Characters</a>
-  <a href="contact.php">Contact</a>
   <a href="about.php">About</a>
   <?php
     if (isset($_SESSION["useruid"])) {
